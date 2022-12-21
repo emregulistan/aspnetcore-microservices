cf3b460c9f5b3c9594d837d2d6d48f66f901da5a
emregulistan <emregulistan1@gmail.com>
2022-12-21 23:44:54 +0300

    catalog.api with mongodb part 1

---
+++ src/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase("DatabaseSettings:DatabaseName");
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
