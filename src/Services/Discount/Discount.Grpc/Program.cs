8729f4658646ff7bfb8bc9f6c55fef2adbfa0ddc
emregulistan <emregulistan1@gmail.com>
2023-01-01 21:47:07 +0300

    discount.grpc microservices grpc communication

---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,22 @@
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MigrateDatabase<Program>();
+app.MapGrpcService<DiscountService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
