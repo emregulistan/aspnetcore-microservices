7770f406cdc27bce4fe2c48ab7d0dd37ee623c1b
emregulistan <emregulistan1@gmail.com>
2023-01-04 22:57:06 +0300

    clean architecture and cqrs pattern implementation part3

--- src/Services/Ordering/Order.Infrastructure/Persistence/OrderContextSeed.cs
+++ src/Services/Ordering/Order.Infrastructure/Persistence/OrderContextSeed.cs
@@ -24,7 +24,7 @@ namespace Ordering.Infrastructure.Persistence
         {
             return new List<Order>
             {
-                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+                new Order() {UserName = "Username", FirstName = "Name", LastName = "Lastname", EmailAddress = "example@example.com", AddressLine = "Adress", Country = "Turkey", TotalPrice = 350 }
             };
         }
     }
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,13 @@
+using Ordering.API.Extensions;
+using Ordering.Application;
+using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddApplicationServices();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,7 +21,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MigrateDatabase<OrderContext>((context, service) =>
+        {
+            var logger = app.Services.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed
+                    .SeedAsync(context, logger)
+                    .Wait();
+        });
 app.UseAuthorization();
 
 app.MapControllers();
